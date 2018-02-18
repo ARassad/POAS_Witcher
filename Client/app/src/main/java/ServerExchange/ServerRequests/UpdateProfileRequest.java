acfd725b89ad81cd55b04d6a57a281c358801fef
druysh <dryush97@gmail.com>
2018-02-18 19:47:47 +0300

    Update profile - update password in separate method

--- Client/app/src/main/java/ServerExchange/ServerRequests/UpdateProfileRequest.java
+++ Client/app/src/main/java/ServerExchange/ServerRequests/UpdateProfileRequest.java
@@ -1,8 +1,13 @@
 package ServerExchange.ServerRequests;
 
+import android.graphics.Bitmap;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.Base64;
 import java.util.HashMap;
+
+import ServerExchange.Password;
 
 /**
  * Created by Дима on 16.02.2018.
@@ -12,10 +17,10 @@ public class UpdateProfileRequest extends TokenServerRequest {
 
     private String UPDATE_PROFILE_METHOD_NAME = "EditProfile";
 
-    private String name;
-    private String text;
-    private Base64 photo;
-    private String password;
+    private String name = null;
+    private String text = null;
+    private String photo = null;
+    private String password = null;
 
     public UpdateProfileRequest(String address) {super(address);}
 
@@ -24,41 +29,56 @@ public class UpdateProfileRequest extends TokenServerRequest {
     protected ServerMethod getMethod() {
 
         HashMap<String, String> params = new HashMap<>();
-        params.put("about", text);
-        params.put("name", name);
-        //params.put("photo", photo);
-        params.put("password", password);
+        if (text != null && !"".equals(text)) {
+            params.put("about", text);
+            text = null;
+        }
+
+        if (name != null && !"".equals(name)) {
+            params.put("name", name);
+            name = null;
+        }
+
+        if (photo != null && !"".equals(photo)) {
+            params.put("photo", photo);
+            photo = null;
+        }
+
+        if (password != null && !"".equals(password)) {
+            params.put("password", password);
+            password = null;
+        }
 
         return new ServerMethod(UPDATE_PROFILE_METHOD_NAME, params);
     }
 
     class JsonUpdateProfileServerAnswer extends JsonServerAnswer{
-
-        String about;
-        long id;
-        String name;
-        Base64 photo;
-
-
-
         @Override
         public Boolean convert() {
-            return status.equals("OK");
+            return isStatusOk();
         }
     }
 
     @Override
     protected Class<? extends JsonServerAnswer> getJsonAnswerClass() {
-
-
-        return JsonServerAnswer.class;
+        return JsonUpdateProfileServerAnswer.class;
     }
 
-    public void updateProfile(String name, String text, Base64 photo, String password, IServerAnswerHandler onUpdateProfileHandler) throws IOException {
+    public void updateProfile(String name, String text, Bitmap photo, IServerAnswerHandler onUpdateProfileHandler) throws IOException {
         this.name = name;
         this.text = text;
-        this.photo = photo;
-        this.password = password;
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        photo.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream .toByteArray();
+        this.photo  = Base64.encodeToString(byteArray, Base64.DEFAULT);
+
         startRequest(onUpdateProfileHandler);
     }
+
+    public void updatePassword(String password ,IServerAnswerHandler onUpdateProfileHandler){
+        this.password = Password.encode(password);
+        startRequest(onUpdateProfileHandler);
+    }
+
 }
