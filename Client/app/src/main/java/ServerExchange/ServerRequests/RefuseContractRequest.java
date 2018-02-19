859bd58853853099b99b8812bae7907406ff7dc4
DimaDontsov <Dimayahoo@mail.ru>
2018-02-19 10:29:57 +0300

    Добавлены запросы: - "Выбрать ведьмака к контракту" - "Ответ ведьмака по контракту" - "Снять ведьмака в контракте"

---
+++ Client/app/src/main/java/ServerExchange/ServerRequests/RefuseContractRequest.java
@@ -0,0 +1,48 @@
+package ServerExchange.ServerRequests;
+
+
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * Created by Dima on 19.02.2018.
+ */
+
+public class RefuseContractRequest extends TokenServerRequest {
+
+    private String REFUSE_CONTRACT_METHOD_NAME = "RefuseContract";
+
+    private long id_contract;
+
+    public RefuseContractRequest(String address) {super(address);}
+
+
+    @Override
+    protected ServerMethod getMethod() {
+
+        HashMap<String, String> params = new HashMap<>();
+        params.put("id_contract", String.valueOf(id_contract));
+
+        return new ServerMethod(REFUSE_CONTRACT_METHOD_NAME, params);
+    }
+
+    class JsonRefuseContractServerAnswer extends JsonServerAnswer{
+
+        @Override
+        public Boolean convert() {
+            return status.equals("OK");
+        }
+    }
+
+    @Override
+    protected Class<? extends JsonServerAnswer> getJsonAnswerClass() {
+
+
+        return JsonServerAnswer.class;
+    }
+
+    public void RefuseContract(long id_contract, IServerAnswerHandler onRefuseContractHandler) throws IOException {
+        this.id_contract = id_contract;
+        startRequest(onRefuseContractHandler);
+    }
+}
