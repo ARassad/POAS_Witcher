d868967b1796a0dfcc1ee0fdd9d1382240a078e2
Alexander <katyshev_sasha@mail.ru>
2018-02-19 11:21:45 +0300

    Отредактировано окно профиля Сделан тулбар с кнопками меню и редактирования

--- Client/app/src/main/java/prin366_2018/client/ProfileActivity.java
+++ Client/app/src/main/java/prin366_2018/client/ProfileActivity.java
@@ -1,9 +1,11 @@
 package prin366_2018.client;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
@@ -22,6 +24,16 @@ public class ProfileActivity extends AppCompatActivity {
         setButton((Button)findViewById(R.id.button_about), (TextView)findViewById(R.id.text_about));
         setButton((Button)findViewById(R.id.button_advert_story), (TableLayout)findViewById(R.id.table_advert_story));
         setButton((Button)findViewById(R.id.button_advert_story), (TableLayout)findViewById(R.id.table_advert_story));
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/fa-solid-900.ttf");
+
+        Button buttonMenu = (Button)findViewById(R.id.button_menu);
+        buttonMenu.setTypeface(typeface);
+        buttonMenu.setText("\uf0c9");
+
+        Button buttonEdit = (Button)findViewById(R.id.button_edit);
+        buttonEdit.setTypeface(typeface);
+        buttonEdit.setText("\uf303");
     }
 
     private void setButton(Button button, final View v) {
