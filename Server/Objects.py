fc706a4750d3e7dca17a10f9f8984d29416e12ce
Mikhail <mkovalev134@gmail.com>
2018-02-20 14:02:06 +0300

    Add API GetTowns

--- Server/Objects.py
+++ Server/Objects.py
@@ -24,6 +24,7 @@ class ApiMethod(Enum):
     AnswerWitcherInContract = "AnswerWitcherInContract"
     RefuseContract = "RefuseContract"
     GetListComments = "GetListComments"
+    GetTowns = "GetTowns"
 
 
 class Comment(Enum):
