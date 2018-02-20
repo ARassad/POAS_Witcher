e1309796e4b7c20ba009639be67c7667bbf57d67
DimaDontsov <Dimayahoo@mail.ru>
2018-02-20 14:01:19 +0300

    Добавлен запрос AddWitcherInContractRequest(Добавить желающего ведьмака к контракту)

---
+++ Client/app/src/main/java/ServerExchange/ServerRequests/AddWitcherInContractRequest.java
@@ -0,0 +1,49 @@
+package ServerExchange.ServerRequests;
+
+
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * Created by Dima on 20.02.2018.
+ */
+
+public class AddWitcherInContractRequest extends TokenServerRequest {
+
+    private String ADD_WITCHER_IN_CONTRACT_METHOD_NAME = "AddWitcherInContract";
+
+    private long id_contract;
+
+    public AddWitcherInContractRequest(String address) {super(address);}
+    public AddWitcherInContractRequest() {super();}
+
+
+    @Override
+    protected ServerMethod getMethod() {
+
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("id_contract", id_contract);
+
+        return new ServerMethod(ADD_WITCHER_IN_CONTRACT_METHOD_NAME, params);
+    }
+
+    class JsonAddWitcherInContractServerAnswer extends JsonServerAnswer{
+
+        @Override
+        public Boolean convert() {
+            return status.equals("OK");
+        }
+    }
+
+    @Override
+    protected Class<? extends JsonServerAnswer> getJsonAnswerClass() {
+
+
+        return AddWitcherInContractRequest.JsonAddWitcherInContractServerAnswer.class;
+    }
+
+    public void addWitcherInContract(long id_contract, IServerAnswerHandler onAddWitcherInContractHandler) throws IOException {
+        this.id_contract = id_contract;
+        startRequest(onAddWitcherInContractHandler);
+    }
+}
