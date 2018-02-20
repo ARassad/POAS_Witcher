c2fc85db0a356a83e22cb3a436e64b23add8fb6a
druysh <dryush97@gmail.com>
2018-02-20 13:45:54 +0300

    CreateAdvertRequest updated

--- Client/app/src/main/java/ServerExchange/ServerRequests/CreateAdvertRequest.java
+++ Client/app/src/main/java/ServerExchange/ServerRequests/CreateAdvertRequest.java
@@ -1,10 +1,13 @@
 package ServerExchange.ServerRequests;
 
 
+import android.graphics.Bitmap;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.HashMap;
+import java.util.LinkedList;
 
 import ServerExchange.Advert;
 import ServerExchange.ImageConvert;
@@ -23,8 +26,9 @@ public class CreateAdvertRequest extends TokenServerRequest {
 
     private int id_task_located;
     private String text;
+    private String header;
     private int bounty;
-    private ArrayList<Base64> photo;
+    private LinkedList<String> photos;
 
     public CreateAdvertRequest(String serverAddress) {
         super(serverAddress);
@@ -33,16 +37,12 @@ public class CreateAdvertRequest extends TokenServerRequest {
     @Override
     protected ServerMethod getMethod() {
 
-        HashMap<String, String> params = new HashMap<>();
-        params.put("id_task_located",String.valueOf(this.id_task_located));
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("id_task_located",this.id_task_located);
         params.put("text",text);
-        params.put("bounty",String.valueOf(this.bounty));
-        String photos = new String();
-        for (Base64 photo:photo){
-            photos+= ImageConvert.toBase64Str(photo);
-            photos+=" ";
-        }
-        params.put("photo",photos);
+        params.put("header", header);
+        params.put("bounty", this.bounty);
+        params.put("photo", photos);
 
         return new ServerMethod(CREATE_ADVERT_METHOD_NAME, params);
     }
@@ -50,65 +50,30 @@ public class CreateAdvertRequest extends TokenServerRequest {
     @Override
     protected Class<? extends JsonServerAnswer> getJsonAnswerClass() {
 
-        return CreateAdvertRequest.AdvertJsonServerAnswer.class;
+        return CreateAdvertJsonServerAnswer.class;
     }
 
 
-    class AdvertJsonServerAnswer extends JsonServerAnswer{
+    class CreateAdvertJsonServerAnswer extends JsonServerAnswer{
 
-        public class ClientJson{
-            public long id;
-            public String name;
-        }
-        public GetAdvertRequest.AdvertJsonServerAnswer.ClientJson client;
-
-        public class AdvertJson{
-            public int bounty;
-            class Comments{
-                public long count;
-                public long comments[];
-            }
-            public GetAdvertRequest.AdvertJsonServerAnswer.AdvertJson.Comments commentsContract;
-            //TODO: Напомнить про заголовок
-
-            public long id;
-            public String kingdom;
-            public long last_update;
-            public long last_update_status;
-
-            public class PhotosJson{
-                public long count;
-                public long photo[];
-            }
-            public GetAdvertRequest.AdvertJsonServerAnswer.AdvertJson.PhotosJson photoContact;
-            public int status;
-            public String text;
-            public String town;
-        }
-        public GetAdvertRequest.AdvertJsonServerAnswer.AdvertJson object;
-
-        public class WitcherJson{
-            public long id;
-            public String name;
-        }
-        public GetAdvertRequest.AdvertJsonServerAnswer.WitcherJson witcher;
 
         @Override
-        public Advert convert() {
-            java.util.Date dateOfLastUpdate = new java.util.Date(object.last_update);
-            //TODO: Узнать, как приходят фотки
-            Advert advert = new Advert(object.id, "fix it", object.text, null, new Location(object.kingdom, object.town),
-                    object.bounty, client.id, null/*other method*/, witcher.id, Advert.AdvertStatus.fromInt(object.status),
-                    dateOfLastUpdate, null);
-            return advert;
+        public Object convert() {
+            return isStatusOk();
         }
     }
 
-    public void createAdvert(int id_task_located, String text, int bounty, ArrayList<Base64> photo, IServerAnswerHandler onCreateAdvertHandler) throws IOException {
+    public void createAdvert(String header, String text, int bounty, int id_task_located,  ArrayList<Bitmap> photos, IServerAnswerHandler onCreateAdvertHandler)  {
         this.id_task_located = id_task_located;
         this.text = text;
+        this.header = header;
         this.bounty = bounty;
-        this.photo = photo;
+        LinkedList<String> photoStrs = new LinkedList<>();
+        for (Bitmap img : photos){
+            photoStrs.addLast( ImageConvert.toBase64Str(img));
+        }
+
+        this.photos = photoStrs;
         startRequest(onCreateAdvertHandler);
     }
 }
