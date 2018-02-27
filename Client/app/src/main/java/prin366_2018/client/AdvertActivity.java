41f61aff7912c4454ff66066d3ecfc4e6ea5057a
Mikhail <mikhailkovalyov34@gmail.com>
2018-02-27 18:40:23 +0300

    Выход с firebase

--- Client/app/src/main/java/prin366_2018/client/AdvertActivity.java
+++ Client/app/src/main/java/prin366_2018/client/AdvertActivity.java
@@ -20,6 +20,8 @@ import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -423,6 +425,7 @@ public class AdvertActivity extends AppCompatActivity implements NavigationView.
         else if (id == R.id.nav_exit) {
             // TODO: Добавить пере Firebase запрос к серверу на выход из профиля
             // TODO: ExitProfile: token
+            FirebaseAuth.getInstance().signOut();
             Intent intent = new Intent(AdvertActivity.this, LoginActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
