58c184ded0e5f944660cc2cd93a213560594157d
Mikhail <mikhailkovalyov34@gmail.com>
2018-03-03 20:53:16 +0300

    Комментарии стали анонимными

--- Server/Comments.py
+++ Server/Comments.py
@@ -18,7 +18,7 @@ def get_list_comment(cursor, params):
     sel = "Contract"
     if params.get(Comments.Type.value, None) == Comments.Profile.value:
         sel = "Profile"
-    cursor.execute("select text, create_date, id from Comment where \
+    cursor.execute("select text, create_date from Comment where \
                     id_list_comment=(select id_list_comments from {} where id={})"
                    .format(sel, params[Profile.ID.value]))
     row = cursor.fetchall()
@@ -32,12 +32,12 @@ def get_list_comment(cursor, params):
         comm.text = i[0]
         comm.date = i[1]
 
-        cursor.execute("select a.id, a.name, b.photo from Profile as a left join Photo as b on a.id_photo = b.id \
-                        where a.id_list_comments=(select id_list_comment from Comment where id={})".format(i[2]))
-        prof = cursor.fetchone()
-        comm.id_prof = prof[0]
-        comm.name = prof[1]
-        comm.photo = prof[2]
+        #cursor.execute("select a.id, a.name, b.photo from Profile as a left join Photo as b on a.id_photo = b.id \
+        #                where a.id_list_comments=(select id_list_comment from Comment where id={})".format(i[2]))
+        #prof = cursor.fetchone()
+        #comm.id_prof = prof[0]
+        #comm.name = prof[1]
+        #comm.photo = prof[2]
         obj.id_comments[len(obj.id_comments)] = comm
 
     status.object = obj
